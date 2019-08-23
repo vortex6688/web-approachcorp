01f9ccbe36ee044a24a639ace18e9e0fa3963c68
vortex6688 <vortex6688@gmail.com>
2019-08-23 03:00:10 -0400

    reorganize style documents,
    update gulp gulpfile and package.json

--- gulpfile.js
+++ gulpfile.js
@@ -40,7 +40,7 @@ function images() {
 // Compile SASS to CSS
 function css() {
     return gulp
-        .src("./src/scss/**/*.scss")
+        .src("./src/scss/styles.scss")
         .pipe(plumber())
         .pipe(sass({ outputStyle: "expanded" }))
         .pipe(gulp.dest("./docs/css/"))
@@ -80,50 +80,3 @@ exports.clean = clean;
 exports.build = build;
 exports.browserSync = browserSync;
 exports.default = build;
-
-// // Static Server + watching scss/html files
-// // Build Dev 
-// gulp.task('serve', ['build-jekyll-dev', 'js', 'sass', 'images'], function() {
-
-//     browserSync.init({
-//         server: "./docs"
-//     });
-
-//     gulp.watch("src/scss/**/*.*", ['sass']).on('change', browserSync.reload);
-//     gulp.watch("src/images/**/*.*", ['images']).on('change', browserSync.reload);
-//     gulp.watch("src/jekyll/**/*.html", ['build-jekyll-dev']).on('change', browserSync.reload);
-//     gulp.watch("docs/**/*.html", ['build-jekyll-dev']).on('change', browserSync.reload);
-// });
-
-// // Build production version for GH pages
-// gulp.task('prod', ['build-jekyll-prod', 'js', 'sass', 'images']);
-
-// gulp.task('js', function(){
-//     return gulp.src('src/js/**/*.js')
-//       .pipe(gulp.dest('docs/js'))
-//   });
-
-// // Compile sass into CSS & auto-inject into browsers
-// gulp.task('sass', function() {
-//     return gulp.src("src/scss/styles.scss")
-//         .pipe(sass.sync().on('error', sass.logError))
-//         .pipe(gulp.dest("docs/css"))
-//         .pipe(browserSync.stream());
-// });
-
-// // Copy and reduce image size
-// gulp.task('images', () =>
-//     gulp.src('src/images/**/*')
-//         //.pipe(imagemin())
-//         .pipe(gulp.dest('docs/images'))
-//         .pipe(browserSync.stream())
-// );
-
-// // Build Jekyll Dev
-// gulp.task('build-jekyll-dev', shell.task(['bundle exec jekyll build --baseurl "" --incremental']));
-
-// // Build Jekyll Prod
-// gulp.task('build-jekyll-prod', shell.task(['bundle exec jekyll build --baseurl "/web-approach-corp" --incremental']));
-
-// // Project Build Options
-// gulp.task('default', ['serve']);
